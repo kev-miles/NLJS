69ff06178e1c0d97c1691e2d38c1093e2d1e7402
Kevin Miles <kev-miles@hotmail.com>
2022-06-12 09:18:12 +0100

    initial commit

---
+++ js/nl.js
@@ -0,0 +1,21 @@
+const SCREEN_WIDTH = window.innerWidth;
+const SCREEN_HEIGHT = window.innerHeight;
+
+const canvas = document.createElement("canvas");
+canvas.setAttribute("width", SCREEN_WIDTH);
+canvas.setAttribute("height", SCREEN_HEIGHT);
+document.body.appendChild(canvas);
+
+const context = canvas.getContext("2d");
+
+const TICK = 30;
+
+function main(){
+    console.log("Main executed");
+    clearScreen();
+}
+
+function clearScreen(){
+    context.fillStyle = "magenta";
+    context.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+}
