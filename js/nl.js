80ca3ca7b0dc5ff7f78cacdec414cf187ff4a8b2
Kevin Miles <kev-miles@hotmail.com>
2022-06-18 11:00:09 +0100

    removed duplicated code in color operations

--- js/nl.js
+++ js/nl.js
@@ -46,39 +46,45 @@ function operateOnColors(rgbColor1, rgbColor2, operation) {
 
     switch(operations[operation]) {
         case 1:
-            return add(normalizedColor1, normalizedColor2);
+            return normalizedColorToByte(add(normalizedColor1, normalizedColor2));
         case 2:
-            return mul(normalizedColor1, normalizedColor2);
+            return normalizedColorToByte(mul(normalizedColor1, normalizedColor2));
         case 3:
-            return sub(normalizedColor1, normalizedColor2);
+            return normalizedColorToByte(sub(normalizedColor1, normalizedColor2));
         case 4:
-            return div(normalizedColor1, normalizedColor2);
+            return normalizedColorToByte(div(normalizedColor1, normalizedColor2));
+        default:
+            return defineRGBColor(0,0,0);
       }
 }
 
 function add(rgbColor1, rgbColor2){
-    return defineRGBColor(
-        normalizedChannelToByte((rgbColor1.r + rgbColor2.r)), 
-        normalizedChannelToByte((rgbColor1.g + rgbColor2.g)), 
-        normalizedChannelToByte((rgbColor1.b + rgbColor2.b)));
+    return {
+        'r': (rgbColor1.r + rgbColor2.r),
+        'g': (rgbColor1.g + rgbColor2.g),
+        'b': (rgbColor1.b + rgbColor2.b)
+    }
 }
 function mul(rgbColor1, rgbColor2){
-    return defineRGBColor(
-        normalizedChannelToByte((rgbColor1.r * rgbColor2.r)), 
-        normalizedChannelToByte((rgbColor1.g * rgbColor2.g)), 
-        normalizedChannelToByte((rgbColor1.b * rgbColor2.b)));
+    return {
+        'r': (rgbColor1.r * rgbColor2.r),
+        'g': (rgbColor1.g * rgbColor2.g),
+        'b': (rgbColor1.b * rgbColor2.b)
+    }
 }
 function sub(rgbColor1, rgbColor2){
-    return defineRGBColor(
-        normalizedChannelToByte((rgbColor1.r - rgbColor2.r)), 
-        normalizedChannelToByte((rgbColor1.g - rgbColor2.g)), 
-        normalizedChannelToByte((rgbColor1.b - rgbColor2.b)));
+    return {
+        'r': (rgbColor1.r - rgbColor2.r),
+        'g': (rgbColor1.g - rgbColor2.g),
+        'b': (rgbColor1.b - rgbColor2.b)
+    }
 }
 function div(rgbColor1, rgbColor2){
-    return defineRGBColor(
-        normalizedChannelToByte((rgbColor1.r / rgbColor2.r)), 
-        normalizedChannelToByte((rgbColor1.g / rgbColor2.g)), 
-        normalizedChannelToByte((rgbColor1.b / rgbColor2.b)));
+    return {
+        'r': (rgbColor1.r / rgbColor2.r),
+        'g': (rgbColor1.g / rgbColor2.g),
+        'b': (rgbColor1.b / rgbColor2.b)
+    }
 }
 
 function normalizeColor(color){
@@ -89,8 +95,17 @@ function normalizeColor(color){
     };
 }
 
+function normalizedColorToByte(color){
+    return {
+        'r' : normalizedChannelToByte(color.r),
+        'g' : normalizedChannelToByte(color.g),
+        'b' : normalizedChannelToByte(color.b)
+    };
+}
+
 function normalizedChannelToByte(value){
-    return value * 255 / 1;
+    let checkedValue = value > 1 ? 1 : value < 0 ? 0 : value;
+    return checkedValue * 255 / 1;
 }
 
 function normalizeChannelValue(value) {
